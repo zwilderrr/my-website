013755069c3fecf0d254adb98a9f2b70384286ce
Zack Wilder <zwilder@gmail.com>
2020-07-03 19:05:51 -0400

    typo

--- src/info/aboutMe.js
+++ src/info/aboutMe.js
@@ -1,2 +1,2 @@
 export const aboutMe =
-  "I absolutely solving problems, and there’s nothing more satisfying to me than developing a product that is powerful, has a UX that’s smooth as butter, and looks absolutely stunning. I’m naturally curious and inquisitive, and I seek out constructive criticism.";
+  "I absolutely love solving problems, and there’s nothing more satisfying to me than developing a product that is powerful, has a UX that’s smooth as butter, and looks absolutely stunning. I’m naturally curious and inquisitive, and I seek out constructive criticism.";
