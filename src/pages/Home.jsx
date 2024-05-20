932b3ebd7f6d70fc03e8753041cfe460aecc2dba
zack wilder <zwilder@gmail.com>
2024-05-20 15:47:57 -0400

    add greetme and styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,41 +4,52 @@ import { ExternalLinkIcon } from "../assets/ExternalLinkIcon";
 import { useNavigate } from "react-router-dom";
 
 export function Home() {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	return (
-		<div className="home">
-			<div className="card-wrapper">
-				<div onClick={() => navigate("/cv")} className="card">
-					<div className="card-content">CV</div>
-				</div>
+  return (
+    <div className="home">
+      <div className="card-wrapper">
+        <div onClick={() => navigate("/cv")} className="card">
+          <div className="card-content">CV</div>
+        </div>
 
-				<div
-					onClick={() =>
-						window.open("https://blobstars.io", "_blank", "noopener,noreferrer")
-					}
-					className="card blobstars"
-					style={{ position: "relative" }}
-				>
-					<ExternalLinkIcon />
-					<div className="card-content">BlobStarsNFT</div>
-				</div>
+        <div
+          onClick={() =>
+            window.open("https://blobstars.io", "_blank", "noopener,noreferrer")
+          }
+          className="card blobstars"
+          style={{ position: "relative" }}
+        >
+          <ExternalLinkIcon />
+          <div className="card-content">BlobStarsNFT</div>
+        </div>
 
-				<div
-					onClick={() =>
-						window.open(
-							"https://foundation.app/collection/nonart",
-							"_blank",
-							"noopener,noreferrer"
-						)
-					}
-					className="card nonart"
-					style={{ position: "relative" }}
-				>
-					<ExternalLinkIcon />
-					<div className="card-content">nonart (soon)</div>
-				</div>
-			</div>
-		</div>
-	);
+        <div
+          onClick={() =>
+            window.open("https://getgreet.me", "_blank", "noopener,noreferrer")
+          }
+          className="card greetme"
+          style={{ position: "relative" }}
+        >
+          <ExternalLinkIcon />
+          <div className="card-content">GreetMe</div>
+        </div>
+
+        <div
+          onClick={() =>
+            window.open(
+              "https://foundation.app/collection/nonart",
+              "_blank",
+              "noopener,noreferrer"
+            )
+          }
+          className="card nonart"
+          style={{ position: "relative" }}
+        >
+          <ExternalLinkIcon />
+          <div className="card-content">nonart (soon)</div>
+        </div>
+      </div>
+    </div>
+  );
 }
