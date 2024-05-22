839ecca379e470552a0c63cf7dafa666b452aa92
zack wilder <zwilder@gmail.com>
2024-05-22 16:34:07 -0400

    remove senior

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,27 +4,27 @@ import React from "react";
 import { Typewriter } from "react-simple-typewriter";
 
 function Header() {
-	return (
-		<div className="name" onClick={() => (window.location.href = "/")}>
-			<div>
-				<Typewriter
-					words={["", "Zack Wilder"]}
-					typeSpeed={80}
-					delaySpeed={300}
-				/>
-			</div>
-			<div className="title">
-				<Typewriter
-					words={["", "senior software engineer"]}
-					delaySpeed={1600}
-					typeSpeed={50}
-				/>
-			</div>
-			<div className="title blue">
-				<Typewriter words={["", "", "."]} delaySpeed={1400} />
-			</div>
-		</div>
-	);
+  return (
+    <div className="name" onClick={() => (window.location.href = "/")}>
+      <div>
+        <Typewriter
+          words={["", "Zack Wilder"]}
+          typeSpeed={80}
+          delaySpeed={300}
+        />
+      </div>
+      <div className="title">
+        <Typewriter
+          words={["", "software engineer"]}
+          delaySpeed={1600}
+          typeSpeed={50}
+        />
+      </div>
+      <div className="title blue">
+        <Typewriter words={["", "", "."]} delaySpeed={1400} />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
